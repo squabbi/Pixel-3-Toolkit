12acadd0ce5e7c80e109838fdb6e644f147a62d3
Squabbi <squabbi.doge@gmail.com>
2019-01-25 12:58:15 +1100

    Added constant strings

---
+++ Pixel 3 Toolkit/Pixel 3 Toolkit/Util/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pixel_3_Toolkit.Util
+{
+    public static class Constants
+    {
+        public static readonly string factoryImageJson = "https://raw.githubusercontent.com/squabbi/Toolkit-Files/master/dogestep/bluecross/factory-images.json";
+        public static readonly string clientJson = "https://raw.githubusercontent.com/squabbi/Toolkit-Files/master/dogestep/bluecross/client.json";
+    }
+}
