9f1b32bcf67bd8ffd1668348759ef779f0b90563
Squabbi <squabbi.doge@gmail.com>
2019-01-25 14:32:07 +1100

    Modified assembly info

--- Pixel 3 Toolkit/Pixel 3 Toolkit/Properties/AssemblyInfo.cs
+++ Pixel 3 Toolkit/Pixel 3 Toolkit/Properties/AssemblyInfo.cs
@@ -8,10 +8,10 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Pixel 3 Toolkit")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Toolkit for the Pixel 3 and Pixel 3 XL.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Pixel 3 Toolkit")]
+[assembly: AssemblyCompany("Squabbi")]
+[assembly: AssemblyProduct("Dogestep3")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -52,4 +52,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1")]
