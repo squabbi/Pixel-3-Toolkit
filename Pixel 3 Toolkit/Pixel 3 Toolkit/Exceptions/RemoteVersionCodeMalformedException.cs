b3a7ffdbf4224c10f3a5a954e3c39524061585cb
Squabbi <squabbi@users.noreply.github.com>
2019-01-29 20:15:02 +1100

    Added tools and exceptions

---
+++ Pixel 3 Toolkit/Pixel 3 Toolkit/Exceptions/RemoteVersionCodeMalformedException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pixel_3_Toolkit.Exceptions
+{
+    public class RemoteVersionCodeMalformedException : Exception
+    {
+        public RemoteVersionCodeMalformedException()
+        {
+        }
+
+        public RemoteVersionCodeMalformedException(string message) : base(message)
+        {
+        }
+
+        public RemoteVersionCodeMalformedException(string message, Exception inner)
+        : base(message, inner)
+        {
+        }
+    }
+}
---
+++ Pixel 3 Toolkit/Pixel 3 Toolkit/Util/Tools.cs
@@ -0,0 +1,67 @@
+﻿using Pixel_3_Toolkit.Models;
+using Pixel_3_Toolkit.Properties;
+
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Pixel_3_Toolkit.Util
+{
+    public sealed class Tools
+    {
+        private static readonly Tools instance = new Tools();
+
+        public enum VersionCheckResult
+        {
+            OLDER,
+            MATCH,
+            NEWER
+        }
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static Tools()
+        {
+        }
+
+        private Tools()
+        {
+            
+        }
+
+        public VersionCheckResult CheckVersion(Toolkit toolkit)
+        {
+            throw new Exceptions.RemoteVersionCodeMalformedException("hello");
+
+            int remoteVersion = toolkit.VersionCode;
+            // Determine client version
+            int localVersion = int.Parse(FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion);
+
+            if (remoteVersion > localVersion)
+            {
+                return VersionCheckResult.NEWER;
+            }
+            else if (remoteVersion == localVersion)
+            {
+                return VersionCheckResult.MATCH;
+            }
+            else if (remoteVersion < localVersion)
+            {
+                return VersionCheckResult.NEWER;
+            }
+            else
+            {
+                throw new Exceptions.RemoteVersionCodeMalformedException(toolkit.VersionCode.ToString());
+            }
+        }
+
+        // TODO: Make one for the images versioncode
+
+        public static Tools Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
